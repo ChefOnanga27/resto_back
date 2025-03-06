42a2c9181e96c1bed740aab6b836ef29e80bf7b5
ChefOnanga <nangbiteghe@gmail.com>
2025-03-06 08:56:37 +0100

    5

--- controllers/users.js
+++ controllers/users.js
@@ -51,7 +51,7 @@ exports.loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: '1h' } // Durée de validité du token
+      { expiresIn: '24h' } // Durée de validité du token
     );
 
     res.status(200).json({ token });
